cb8ded3e99984e11ae083a5544103e3e9ee527b7
SuJeong68 <tidh1640@naver.com>
2023-01-03 10:51:33 +0900

    Initial Commit

---
+++ src/main/java/shop/yesaladin/yesaladinjenkinssujeong/YesaladinJenkinsSujeongApplication.java
@@ -0,0 +1,13 @@
+package shop.yesaladin.yesaladinjenkinssujeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesaladinJenkinsSujeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesaladinJenkinsSujeongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/yesaladin/yesaladinjenkinssujeong/YesaladinJenkinsSujeongApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.yesaladin.yesaladinjenkinssujeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesaladinJenkinsSujeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
